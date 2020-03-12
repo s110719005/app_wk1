7d55efc4f9d4053f84d250b40ca85141eaa47b10
Alice Wang <alicewang@Alicede-MacBook-Pro-2.local>
2020-03-12 22:38:05 +0800

    Hello RN

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>110719005 王思凡</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+});
